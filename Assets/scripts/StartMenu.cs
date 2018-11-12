63f276ede3aec7c776127be7a08003f0aad99563
Gaolatlhwe SEBAETSE <gsebaets@e5r1p10.wethinkcode.co.za>
2018-11-12 10:15:04 +0200

    init commit

---
+++ Assets/scripts/StartMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEditor;
+
+public class StartMenu : MonoBehaviour {
+
+	public Button load;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		load.onClick.AddListener(loadOnClick);
+	}
+
+	void loadOnClick(){
+        EditorUtility.DisplayPopupMenu(new Rect(Input.mousePosition.x, Input.mousePosition.y, 0, 0), "Assets/", null);
+	}
+}
